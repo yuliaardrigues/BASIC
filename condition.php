fcb1029189050218a6bf52f517eaae8ba2e1d093
yulia <yuliarodrigues@aluno.unifapce.edu.br>
2023-06-23 12:24:01 -0300

    condiçao utilizando switch

--- condition.php
+++ condition.php
@@ -2,13 +2,22 @@
 
 $tipo = 'Video';
 
-if ($tipo == 'Foto'){
-    echo 'exibindo uma foto';
-}
+//if ($tipo == 'Foto'){
+    //echo 'exibindo uma foto';}
 
-if ($tipo == 'Texto'){
-    echo 'exibindo um Texto';
-}
-if ($tipo == 'Video'){
-    echo 'exibindo um Video';
-}
+//if ($tipo == 'Texto'){
+    //echo 'exibindo um Texto';}
+//if ($tipo == 'Video'){
+    //echo 'exibindo um Video';}
+
+switch($tipo){
+    case 'Foto':
+        echo 'Exibindo FOTO';
+         break;
+    case 'Texto':
+        echo 'Exibindo TEXTO';
+         break;
+    case 'Video':
+        echo 'Exibindo VIDEO';
+         break;
+    }
